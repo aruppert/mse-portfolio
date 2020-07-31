0fae0ed43a0350c3908fdab31d34c95d8aa1dc14
Alexander Ruppert <aruppert@pm.me>
2020-07-31 08:06:06 +0200

    Add header with styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { ThemeProvider } from "emotion-theming";
+import dark from "./themes/dark";
+import Header from "./components/Header";
+import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello people of the internet</h1>
-      </header>
-    </div>
+    <ThemeProvider theme={dark}>
+      <Router>
+        <GlobalStyles />
+        <Header />
+        <Switch>
+          <h1>Hello people of the internet</h1>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
