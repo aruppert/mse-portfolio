c9cd11b3895ceee79addbcf5709c013095f3ac62
Alexander Ruppert <aruppert@pm.me>
2020-09-03 17:44:36 +0200

    Update logo (#13)

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@ import React from "react";
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import DropdownMenu from "./DropdownMenu";
+import logo from "../graphics/logo.svg";
 
 const Headerbar = styled.div`
   background: #231d24;
   height: 48px;
   position: fixed;
+  display: flex;
   width: 100%;
   z-index: 300;
 `;
@@ -19,6 +21,8 @@ const Navbar = styled.nav`
   height: 100%;
   margin: 0 5px;
   display: flex;
+  padding: 0 1rem;
+  flex-grow: 1;
   justify-content: space-between;
   font-weight: 400;
 `;
@@ -30,6 +34,12 @@ const Navbutton = styled(Link)`
   &::first-letter {
   }
   color: ${(props) => props.theme.colors.prim};
+`;
+
+const LogoLink = styled(Navbutton)`
+  display: flex;
+  align-items: flex-end;
+  margin: 0 0 0 0.8rem;
 `;
 
 const Wrapper = styled.button`
@@ -60,8 +70,10 @@ export default function Header() {
   return (
     <>
       <Headerbar>
+        <LogoLink to="/">
+          <img src={logo} alt="Logo" />
+        </LogoLink>
         <Navbar>
-          <Navbutton to="/">Logo.</Navbutton>
           <Wrapper>
             Work.
             <DropdownMenu />
