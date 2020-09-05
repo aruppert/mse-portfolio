d9402d575bb9f337c6f46b6ec88ff665580ae684
Alexander Ruppert <aruppert@pm.me>
2020-09-05 11:48:23 +0200

    Fix footer to bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,6 @@ import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 
 const Navbar = styled.nav`
-  position: absolute;
-  bottom: -80;
-  right: 0;
-  /* height: 100%; */
   margin: 3rem 0.5rem 0;
   display: flex;
   font-weight: 400;
