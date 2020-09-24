a38f44a308f2be67716fdd0f02e608b04b5719d9
Alexander Ruppert <aruppert@pm.me>
2020-09-24 12:36:04 +0200

    Rename nav to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 // import { Link } from "react-router-dom";
 
-const Navbar = styled.nav`
+const Navbar = styled.footer`
   display: flex;
   justify-content: flex-end;
   margin: 4rem 1.2rem 0.5rem;
