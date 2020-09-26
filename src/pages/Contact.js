616b18f378a5ddfb319345822de4bf1a81b7f024
Alexander Ruppert <aruppert@pm.me>
2020-09-26 10:08:07 +0200

    Add front-end part of form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,12 +3,13 @@ import styled from "@emotion/styled";
 import ContactSVG from "../graphics/ContactSVG.js";
 import { useForm } from "react-hook-form";
 import isEmail from "validator/lib/isEmail";
+import ActionIcon from "../graphics/ActionIcon.js";
+import { css } from "@emotion/core";
 
 const Container = styled.main`
   display: flex;
   flex-flow: column nowrap;
   flex-grow: 1;
-  height: 80vh;
 `;
 
 const Name = styled.h1`
@@ -35,10 +36,64 @@ const Form = styled.form`
   display: flex;
   flex-flow: column;
   justify-content: space-between;
-  min-height: 500px;
-  width: 90%;
+  min-height: 380px;
+  width: 80%;
   margin: 2rem auto 0;
 `;
+
+const InputStyles = css`
+  background: var(--dark-form);
+  color: var(--dark-txt);
+  height: 2rem;
+  border-radius: 10px;
+  width: 100%;
+  margin: 0 auto;
+  ::placeholder {
+    color: var(--dark-txt);
+    opacity: 1;
+    font-size: 1rem;
+    font-weight: 200;
+    padding: 0 0.2rem;
+  }
+`;
+
+const NameInput = styled.input`
+  ${InputStyles};
+  border-color: ${(props) => (props.errors.name ? "var(--dark-danger)" : "")};
+`;
+
+const MiscInput = styled.input`
+  ${InputStyles};
+`;
+
+const EmailInput = styled.input`
+  ${InputStyles};
+  border-color: ${(props) => (props.errors.email ? "var(--dark-danger)" : "")};
+`;
+
+const MessageInput = styled.textarea`
+  ${InputStyles};
+  border-color: ${(props) =>
+    props.errors.message ? "var(--dark-danger)" : ""};
+  height: 10rem;
+  ::placeholder {
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+  }
+`;
+
+const SubmitButton = styled.button`
+  align-self: flex-end;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 5.5rem;
+  border: none;
+  background: inherit;
+  color: var(--dark-sec);
+  font-size: 1.2rem;
+`;
+
 export default function Contact() {
   const { register, handleSubmit, errors, formState } = useForm({
     mode: "onBlur",
@@ -59,7 +114,7 @@ export default function Contact() {
         </a>
       </ContactDetails>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <input
+        <NameInput
           ref={register({
             required: true,
             minLength: 2,
@@ -67,35 +122,40 @@ export default function Contact() {
             pattern: /^[A-Za-z]+$/i,
           })}
           name="name"
-          placeholder="Name*"
+          placeholder="Name *"
+          errors={errors}
         />
-        <input
+        <MiscInput
           ref={register({ maxLength: 500 })}
           name="subject"
           placeholder="Subject"
+          errors={errors}
         />
-        <input
+        <EmailInput
           ref={register({
             required: true,
             validate: (input) => isEmail(input),
           })}
-          style={{ borderColor: errors.email && "red" }}
           name="email"
-          placeholder="E-Mail*"
+          placeholder="E-Mail *"
+          errors={errors}
         />
-        <input
+        <MiscInput
           ref={register({ maxLength: 20 })}
           name="tel"
           placeholder="Telefon"
+          errors={errors}
         />
-        <input
-          ref={register({ required: true, minLength: 10, maxLength: 10000 })}
+        <MessageInput
+          ref={register({ required: true, minLength: 2, maxLength: 10000 })}
           name="message"
-          placeholder="Message*"
+          placeholder="Message *"
+          errors={errors}
         />
-        <button type="submit" disabled={formState.isSubmitting}>
+        <SubmitButton type="submit" disabled={formState.isSubmitting}>
+          <ActionIcon />
           Send
-        </button>
+        </SubmitButton>
       </Form>
     </Container>
   );
