256a2c3504496c6f36a08ec6b9c0ca43d59a416a
Alexander Ruppert <aruppert@pm.me>
2020-10-08 10:56:00 +0200

    Add media queries

---
+++ src/components/MediaQueries.js
@@ -0,0 +1,16 @@
+const mq = (n) => {
+  const bpArray = Object.keys(bp).map((key) => [key, bp[key]]);
+
+  const [result] = bpArray.reduce((acc, [name, size]) => {
+    if (n === name) return [...acc, `@media (min-width: ${size}px)`];
+    return acc;
+  }, []);
+
+  return result;
+};
+
+const bp = {
+  medium: 1050,
+};
+
+export { mq, bp };
