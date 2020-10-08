99e346389d8eb53207de738fedb3a818342b2628
Alexander Ruppert <aruppert@pm.me>
2020-10-08 10:56:30 +0200

    Make CV responsive

--- src/components/CV.js
+++ src/components/CV.js
@@ -2,89 +2,133 @@ import React from "react";
 import styled from "@emotion/styled";
 import ActionIcon from "../graphics/ActionIcon";
 import { Link } from "react-router-dom";
+import { mq } from "../components/MediaQueries";
 
 export default function CV() {
   return (
     <>
-      <CVHeadingWrapper>
-        <CVHeadingPlaceholder />
-        <CVHeading>Education</CVHeading>
-      </CVHeadingWrapper>
+      <SectionContainer>
+        <SectionTitle>Long story short</SectionTitle>
+        <CVHeadingWrapper>
+          <CVHeadingPlaceholder />
+          <CVHeading>Education</CVHeading>
+        </CVHeadingWrapper>
 
-      <CVEntryContainer>
-        <CVEntryYear>2020</CVEntryYear>
-        <CVEntryText>
-          Courses in UX and UI design at cimdata academy, Cologne 
-        </CVEntryText>
-      </CVEntryContainer>
+        <CVEntryContainer>
+          <CVEntryYear>2020</CVEntryYear>
+          <CVEntryText>
+            Courses in UX and UI design at cimdata academy, Cologne 
+          </CVEntryText>
+        </CVEntryContainer>
 
-      <CVEntryContainer>
-        <CVEntryYear>2014</CVEntryYear>
-        <CVEntryText>Exchange semester at ZHDK, Zurich</CVEntryText>
-      </CVEntryContainer>
+        <CVEntryContainer>
+          <CVEntryYear>2014</CVEntryYear>
+          <CVEntryText>Exchange semester at ZHDK, Zurich</CVEntryText>
+        </CVEntryContainer>
 
-      <CVEntryContainer>
-        <CVEntryYear>2010 - 2015</CVEntryYear>
-        <CVEntryText>
-          Bachelor degree in Integrated Design at KISD, Cologne
-        </CVEntryText>
-      </CVEntryContainer>
+        <CVEntryContainer>
+          <CVEntryYear>2010 - 2015</CVEntryYear>
+          <CVEntryText>
+            Bachelor degree in Integrated Design at KISD, Cologne
+          </CVEntryText>
+        </CVEntryContainer>
 
-      <CVEntryContainer>
-        <CVEntryYear>2005 - 2008 </CVEntryYear>
-        <CVEntryText>
-          Apprenticeship as Mediadesigner in audiovisual Media, Fraunhofer IAIS
-        </CVEntryText>
-      </CVEntryContainer>
-      <CVEntryContainer>
-        <CVEntryYear>2004</CVEntryYear>
-        <CVEntryText>High School, Martinus Gymnasium Linz/Rhein</CVEntryText>
-      </CVEntryContainer>
+        <CVEntryContainer>
+          <CVEntryYear>2005 - 2008 </CVEntryYear>
+          <CVEntryText>
+            Apprenticeship as Mediadesigner in audiovisual Media, Fraunhofer
+            IAIS
+          </CVEntryText>
+        </CVEntryContainer>
 
-      <CVHeadingWrapper>
-        <CVHeadingPlaceholder />
-        <CVHeading>Work Experience</CVHeading>
-      </CVHeadingWrapper>
+        <CVEntryContainer>
+          <CVEntryYear>2004</CVEntryYear>
+          <CVEntryText>High School, Martinus Gymnasium Linz/Rhein</CVEntryText>
+        </CVEntryContainer>
+      </SectionContainer>
 
-      <CVEntryContainer>
-        <CVEntryYear>2016</CVEntryYear>
-        <CVEntryText>Social media editor, probono TV, Cologne </CVEntryText>
-      </CVEntryContainer>
-      <CVEntryContainer>
-        <CVEntryYear>since 2014</CVEntryYear>
-        <CVEntryText>Freelance designer and photographer </CVEntryText>
-      </CVEntryContainer>
-      <CVEntryContainer>
-        <CVEntryYear>2014 - 2017</CVEntryYear>
-        <CVEntryText>
-          Freelance graphic designer and photographer, Lichtfaktor, Cologne
-        </CVEntryText>
-      </CVEntryContainer>
-      <CVEntryContainer>
-        <CVEntryYear>2013</CVEntryYear>
-        <CVEntryText>
-          Graphic designer and photographer, Stu Shapiro, Cape Town
-        </CVEntryText>
-      </CVEntryContainer>
-      <CVEntryContainer>
-        <CVEntryYear>2013</CVEntryYear>
-        <CVEntryText>
-          Social design project „Come To Gather“, Cape Town
-        </CVEntryText>
-      </CVEntryContainer>
-      <CVEntryContainer>
-        <CVEntryYear>2008 - 2009</CVEntryYear>
-        <CVEntryText>
-          Mediadesigner in audiovisual Media, Fraunhofer IAIS
-        </CVEntryText>
-      </CVEntryContainer>
-      <ContactLink to="/contact">
-        <ActionIcon />
-        <ContactLinkText>Interested? Contact me here.</ContactLinkText>
-      </ContactLink>
+      <SecondPartContainer>
+        <SecondPartTitle>Long story short Part II</SecondPartTitle>
+
+        <CVHeadingWrapper>
+          <CVHeadingPlaceholder />
+          <CVHeading>Work Experience</CVHeading>
+        </CVHeadingWrapper>
+
+        <CVEntryContainer>
+          <CVEntryYear>2016</CVEntryYear>
+          <CVEntryText>Social media editor, probono TV, Cologne </CVEntryText>
+        </CVEntryContainer>
+
+        <CVEntryContainer>
+          <CVEntryYear>since 2014</CVEntryYear>
+          <CVEntryText>Freelance designer and photographer </CVEntryText>
+        </CVEntryContainer>
+
+        <CVEntryContainer>
+          <CVEntryYear>2014 - 2017</CVEntryYear>
+          <CVEntryText>
+            Freelance graphic designer and photographer, Lichtfaktor, Cologne
+          </CVEntryText>
+        </CVEntryContainer>
+
+        <CVEntryContainer>
+          <CVEntryYear>2013</CVEntryYear>
+          <CVEntryText>
+            Graphic designer and photographer, Stu Shapiro, Cape Town
+          </CVEntryText>
+        </CVEntryContainer>
+
+        <CVEntryContainer>
+          <CVEntryYear>2013</CVEntryYear>
+          <CVEntryText>
+            Social design project „Come To Gather“, Cape Town
+          </CVEntryText>
+        </CVEntryContainer>
+
+        <CVEntryContainer>
+          <CVEntryYear>2008 - 2009</CVEntryYear>
+          <CVEntryText>
+            Mediadesigner in audiovisual Media, Fraunhofer IAIS
+          </CVEntryText>
+        </CVEntryContainer>
+
+        <ContactLink to="/contact">
+          <ActionIcon />
+          <ContactLinkText>Interested? Contact me here.</ContactLinkText>
+        </ContactLink>
+      </SecondPartContainer>
     </>
   );
 }
+
+const SectionContainer = styled.section`
+  width: 90%;
+  max-width: 25em;
+  height: 30em;
+  border-top: 3px solid #fff;
+  margin: 2rem 0;
+  ${mq("medium")} {
+    margin: 4em 2em;
+  }
+`;
+
+const SecondPartContainer = styled(SectionContainer)`
+  border-top: 3px solid ${(props) => props.theme.colors.bg};
+`;
+
+const SectionTitle = styled.h2`
+  margin-top: 0.2rem;
+  margin-bottom: 1rem;
+  font-size: 1.2rem;
+  font-weight: 400;
+  line-height: 20px;
+  text-transform: uppercase;
+`;
+
+const SecondPartTitle = styled(SectionTitle)`
+  color: ${(props) => props.theme.colors.bg};
+`;
 
 const CVHeadingWrapper = styled.div`
   display: flex;
@@ -127,13 +171,13 @@ const ContactLink = styled(Link)`
   display: flex;
   justify-content: flex-end;
   width: 100%;
-  margin: 0.6rem 0 0;
+  margin: 3.5rem 0;
   text-decoration: none;
 `;
 
 const ContactLinkText = styled.p`
   width: fit-content;
-  margin: 0 5px;
+  margin: 0 0.3em;
   color: ${(props) => props.theme.colors.sec};
   font-weight: 600;
 `;
