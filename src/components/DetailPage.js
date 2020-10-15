e8e3892bd9304f6908a36c8ae5834cf66f0f5bde
Alexander Ruppert <aruppert@pm.me>
2020-10-15 13:08:24 +0200

    Make detail pages responsive with grid

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { mq } from "../components/MediaQueries";
 
 const Container = styled.div`
   display: flex;
@@ -6,14 +7,31 @@ const Container = styled.div`
   justify-content: center;
   flex-grow: 1;
   height: 100%;
+  ${mq("medium")}{
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-rows: auto;
+  }
 `;
 
+const SectionWrapper = styled.section`
+max-width: 23em;
+margin: auto;
+${mq("medium")} {
+    height:100%;
+  
+  }`
+
 const Teaser = styled.p`
-  width: 90%;
-  padding: 0 16%;
-  margin: 1rem auto 0;
-  font-size: 18px;
-  line-height: 20px;
+  width: 80%;
+    padding: 0 0 0 5%;
+  margin: 1em auto 0;
+  font-size: 1.125em;
+  line-height: 1.25em
+  ${mq("medium")} {
+    padding: 2em 0 0 5%;
+  
+  }
 `;
 const Description = styled.p`
   width: 90%;
@@ -25,8 +43,7 @@ const SVGWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 90%;
   margin: 1rem 0;
 `;
 
-export { Container, Teaser, Description, SVGWrapper };
+export { Container, Teaser, Description, SVGWrapper, SectionWrapper };
