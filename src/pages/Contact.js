1c6907fb690206adab310b0d7f8f2f1596ecd5f5
Alexander Ruppert <aruppert@pm.me>
2021-03-23 13:56:19 +0100

    Fix further vercel problems for deployment (#58)

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,71 +1,71 @@
 import React from "react";
 import styled from "@emotion/styled";
 import ContactSVG from "../graphics/ContactSVG.js";
-import ActionIcon from "../graphics/ActionIcon.js";
-import { css } from "@emotion/core";
-import axios from "axios";
-import { useForm } from "react-hook-form";
-import isEmail from "validator/lib/isEmail";
+// import ActionIcon from "../graphics/ActionIcon.js";
+// import { css } from "@emotion/core";
+// import axios from "axios";
+// import { useForm } from "react-hook-form";
+// import isEmail from "validator/lib/isEmail";
 
 export default function Contact() {
-  const [loading, setLoading] = React.useState(false);
-  const [state, setState] = React.useState({
-    email: "",
-    message: "",
-    name: "",
-    subject: "",
-    tel: "",
-  });
-  const [result, setResult] = React.useState(null);
-  const { register, errors } = useForm({
-    mode: "onBlur",
-  });
+  // const [loading, setLoading] = React.useState(false);
+  // const [state, setState] = React.useState({
+  //   email: "",
+  //   message: "",
+  //   name: "",
+  //   subject: "",
+  //   tel: "",
+  // });
+  // const [result, setResult] = React.useState(null);
+  // const { register, errors } = useForm({
+  //   mode: "onBlur",
+  // });
 
-  const sendEmail = (event) => {
-    event.preventDefault();
-    if (state.name && state.email && state.message) {
-      setLoading(true);
-      axios
-        .post("/send", { ...state })
-        .then((response) => {
-          setResult(response.data);
-          setState({
-            name: "",
-            email: "",
-            subject: "",
-            message: "",
-            tel: "",
-          });
-        })
-        .catch(() => {
-          setResult({
-            success: false,
-            message: "Something went wrong. Reload and try again later",
-          });
-        });
-    }
-  };
+  // const sendEmail = (event) => {
+  //   event.preventDefault();
+  //   if (state.name && state.email && state.message) {
+  //     setLoading(true);
+  //     axios
+  //       .post("/send", { ...state })
+  //       .then((response) => {
+  //         setResult(response.data);
+  //         setState({
+  //           name: "",
+  //           email: "",
+  //           subject: "",
+  //           message: "",
+  //           tel: "",
+  //         });
+  //       })
+  //       .catch(() => {
+  //         setResult({
+  //           success: false,
+  //           message: "Something went wrong. Reload and try again later",
+  //         });
+  //       });
+  //   }
+  // };
 
-  const onInputChange = (event) => {
-    const { name, value } = event.target;
-    setState({
-      ...state,
-      [name]: value,
-    });
-  };
+  // const onInputChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setState({
+  //     ...state,
+  //     [name]: value,
+  //   });
+  // };
 
-  React.useEffect(() => {
-    if (result != null) {
-      setLoading(false);
-    }
-    setState({
-      name: "",
-      email: "",
-      subject: "",
-      message: "",
-      tel: "",
-    });
-  }, [result]);
+  // React.useEffect(() => {
+  //   if (result != null) {
+  //     setLoading(false);
+  //   }
+  //   setState({
+  //     name: "",
+  //     email: "",
+  //     subject: "",
+  //     message: "",
+  //     tel: "",
+  //   });
+  // }, [result]);
 
   return (
     <Container>
@@ -174,76 +174,76 @@ const ContactSVGstyled = styled(ContactSVG)`
   margin: 3rem 1.5rem 1.7rem 0;
 `;
 
-const Form = styled.form`
-  display: flex;
-  flex-flow: column;
-  justify-content: space-between;
-  min-height: 380px;
-  width: 80%;
-  margin: 2rem auto 0;
-`;
+// const Form = styled.form`
+//   display: flex;
+//   flex-flow: column;
+//   justify-content: space-between;
+//   min-height: 380px;
+//   width: 80%;
+//   margin: 2rem auto 0;
+// `;
 
-const InputStyles = css`
-  background: var(--dark-form);
-  color: var(--dark-txt);
-  height: 2rem;
-  border-radius: 10px;
-  width: 100%;
-  margin: 0 auto;
-  ::placeholder {
-    color: var(--dark-txt);
-    opacity: 1;
-    font-size: 1rem;
-    font-weight: 200;
-    padding: 0 0.2rem;
-  }
-`;
+// const InputStyles = css`
+//   background: var(--dark-form);
+//   color: var(--dark-txt);
+//   height: 2rem;
+//   border-radius: 10px;
+//   width: 100%;
+//   margin: 0 auto;
+//   ::placeholder {
+//     color: var(--dark-txt);
+//     opacity: 1;
+//     font-size: 1rem;
+//     font-weight: 200;
+//     padding: 0 0.2rem;
+//   }
+// `;
 
-const NameInput = styled.input`
-  ${InputStyles};
-  border-color: ${(props) => (props.errors.name ? "var(--dark-danger)" : "")};
-`;
+// const NameInput = styled.input`
+//   ${InputStyles};
+//   border-color: ${(props) => (props.errors.name ? "var(--dark-danger)" : "")};
+// `;
 
-const MiscInput = styled.input`
-  ${InputStyles};
-`;
+// const MiscInput = styled.input`
+//   ${InputStyles};
+// `;
 
-const EmailInput = styled.input`
-  ${InputStyles};
-  border-color: ${(props) => (props.errors.email ? "var(--dark-danger)" : "")};
-`;
+// const EmailInput = styled.input`
+//   ${InputStyles};
+//   border-color: ${(props) => (props.errors.email ? "var(--dark-danger)" : "")};
+// `;
 
-const MessageInput = styled.textarea`
-  ${InputStyles};
-  border-color: ${(props) =>
-    props.errors.message ? "var(--dark-danger)" : ""};
-  height: 10rem;
-  ::placeholder {
-    font-family: "Montserrat", sans-serif;
-    font-weight: 400;
-  }
-`;
+// const MessageInput = styled.textarea`
+//   ${InputStyles};
+//   border-color: ${(props) =>
+//     props.errors.message ? "var(--dark-danger)" : ""};
+//   height: 10rem;
+//   ::placeholder {
+//     font-family: "Montserrat", sans-serif;
+//     font-weight: 400;
+//   }
+// `;
 
-const SubmitButton = styled.button`
-  align-self: flex-end;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 5.5rem;
-  border: none;
-  background: inherit;
-  color: var(--dark-sec);
-  font-size: 1.2rem;
-`;
+// const SubmitButton = styled.button`
+//   align-self: flex-end;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   width: 5.5rem;
+//   border: none;
+//   background: inherit;
+//   color: var(--dark-sec);
+//   font-size: 1.2rem;
+// `;
 
-const ResultMessage = styled.p`
-  width: 80%;
-  margin: 2rem auto;
-  text-align: center;
-  color: ${(props) =>
-    props.result ? "var(--dark-sec)" : "var(--dark-danger)"};
-`;
+// const ResultMessage = styled.p`
+//   width: 80%;
+//   margin: 2rem auto;
+//   text-align: center;
+//   color: ${(props) =>
+//     props.result ? "var(--dark-sec)" : "var(--dark-danger)"};
+// `;
 
-const LoadingMessage = styled(ResultMessage)`
-  color: var(--dark-pri);
-`;
+// const LoadingMessage = styled(ResultMessage)`
+//   color: var(--dark-pri);
+// `;
